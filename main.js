f4c19a44b6867aa685fb54c29ddb7df38284edc2
JUANTITO77 <jelvistq77@gmail.com>
2024-06-30 22:11:07 -0500

    subiendo mas cambios

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-const menu = document.getElementById('menuDesple');
-const botonMenu = document.getElementById('img_menu');
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.getElementById('menuDesple');
+    const botonMenu = document.getElementById('img_menu');
+    const menuItems = document.querySelectorAll('#menuDesple li a');
 
-botonMenu.addEventListener('click', () => {
-    menu.classList.toggle('visible');
+    botonMenu.addEventListener('click', () => {
+        menu.classList.toggle('visible');
+    });
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.remove('visible');
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const webRopaImage = document.getElementById('webRopaImage');
+    webRopaImage.addEventListener('click', () => {
+        window.open('https://juantito77.github.io/TiendaDeRopa/', '_blank');
+    });
 });
